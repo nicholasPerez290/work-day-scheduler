3085ffed111d160524fadd85bb0528fc05380211
Nicholas Perez <nicholas.perez290@gmail.com>
2021-06-25 16:06:32 -0400

    add readme, comments, and folders

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # work-day-scheduler
+
+# purpose 
+
+* to create an online day planner that saves your inputs until you clear them
+
+# features 
+
+* Saves typed inputs for each hour with a save button
+
+* has color coded past present and future
+
+* displays current date
+
+# How it works
+
+* Time uses moment
+
+* Using JavaScript moment time is compared to an index to assign past present future classes
+
+* using JavaScript when text is entered and the save button clicked text saved to local storage
+
+# Screenshot
+
+* [](assets\img\screenshot-127.0.0.1_5500-2021.06.25-16_00_43.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
+// current time
 var now = moment()
 console.log(now);
+// self updating time
 setInterval(function(){
     now = moment();
-    $("#currentDay").text(now.format("dddd, MMMM Do, s"));
+    $("#currentDay").text(now.format("dddd, MMMM Do"));
 },1000);
+// saving our save icons and textareas into arrys with corresponding index
 savei = $('i');
-textArea = $('.textarea');
-time = $('.hour');
+textArea = $('.textarea'); 
 textBox = $('.text-holder');
-console.log(time[1])
 console.log(now.format('H'));
+// displaying saved notes
 for(var i = 0; i <= 8; i++){
     var orgIndex = JSON.parse(localStorage.getItem(`index${i}`));
     var orgText = JSON.parse(localStorage.getItem(`text${i}`));
     textArea[i].textContent = orgText
 }
+// using hour value of moment to decide witch to apply past present and future class
 for(var i = 0; i < 24; i++){
     if(i > 8){
 
@@ -35,6 +38,7 @@ for(var i = 0; i < 24; i++){
         currentHour.setAttribute('class','future');
     }
 }
+// saving input to local storage
 savei.on('click', function(event){
     var target = event.target
     var index = target.getAttribute('value')
